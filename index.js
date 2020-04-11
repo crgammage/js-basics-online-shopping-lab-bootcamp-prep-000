67c171f0692b67b0896565ab207628f390bdc5be
masterful-array-3850 <crgammage@gmail.com>
2020-04-11 18:16:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function placeOrder(cardNumber) {
 if (cardNumber === undefined){
   return `Sorry, we don't have a credit card on file for you.`
 } else {
-  return `Your total cost is ${total()}, which will be charged to the ${cardNumber}.`
+  return `Your total cost is $${total()}, which will be charged to the ${cardNumber}.`
   cart = [];
   return cart;
   }
